229b78fa1a113b7130d4db7b93cb533fb565480b
Robert79v <robert-a-r@hotmail.com>
2025-09-07 14:40:29 -0600

    Widgets

---
+++ lib/widgets/home_content_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:pmsn20252/widgets/item_row_widget.dart';
+import 'package:pmsn20252/models/troop_model.dart';
+
+class HomeContent extends StatelessWidget {
+  const HomeContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: troop.length, // tu lista de TroopModel
+      itemBuilder: (context, index) {
+        return ItemRow(troop: troop[index]); // pasamos el troop correspondiente
+      },
+    );
+  }
+}
---
+++ lib/widgets/item_row_widget.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+import 'package:pmsn20252/models/troop_model.dart';
+import 'package:pmsn20252/screens/detail_screen.dart';
+import 'package:pmsn20252/widgets/atributte_widget.dart';
+
+const double degrees2Radians = math.pi / 180.0;
+const double radians2Degrees = 180.0 / math.pi;
+
+double degrees(double radians) => radians * radians2Degrees;
+
+double radians(double degrees) => degrees * degrees2Radians;
+
+class ItemRow extends StatelessWidget {
+  final TroopModel troop;
+  final double rowHeight;
+
+  const ItemRow({
+    required this.troop,
+    this.rowHeight = 282,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: rowHeight,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Transform.translate(
+            offset: Offset(-10, 0),
+            child: Transform(
+              alignment: FractionalOffset.center,
+              transform: Matrix4.identity()
+                ..setEntry(3, 2, 0.01)
+                ..rotateY(radians(1.5)),
+              child: Container(
+                height: 216,
+                margin: EdgeInsets.symmetric(horizontal: 40),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.1),
+                  borderRadius: BorderRadius.all(Radius.circular(22)),
+                ),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(-44, 0),
+            child: Transform(
+              alignment: FractionalOffset.center,
+              transform: Matrix4.identity()
+                ..setEntry(3, 2, 0.01)
+                ..rotateY(radians(8)),
+              child: Container(
+                height: 188,
+                margin: EdgeInsets.symmetric(horizontal: 40),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.4),
+                  borderRadius: BorderRadius.all(Radius.circular(22)),
+                ),
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              Transform.translate(
+                offset: Offset(-30, 0),
+                child: Container(
+                  child: Image.asset(
+                    troop.image,
+                    width: rowHeight,
+                    height: rowHeight,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Container(
+              margin: EdgeInsets.only(right: 58), //58
+              padding: EdgeInsets.symmetric(vertical: 34),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  AttributeWidget(
+                    progress: troop.shield,
+                    child: Image.asset(shield),
+                  ),
+                  AttributeWidget(
+                    progress: troop.health,
+                    child: Image.asset(heart),
+                  ),
+                  AttributeWidget(
+                    progress: troop.attack,
+                    child: Image.asset(weapon),
+                  ),
+                  SizedBox(
+                    height: 32,
+                    child: OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                        foregroundColor: Colors.white, 
+                        side: BorderSide(color: Colors.white, width: 1),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) {
+                            return TroopDetailsPage(troop);
+                          }),
+                        );
+                      },
+                      child: Text(
+                        'See Details',
+                        style: TextStyle(
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
